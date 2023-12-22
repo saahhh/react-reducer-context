f7e635855090a927303ff8b0254824fe2fe8c458
saahhh <143372306+saahhh@users.noreply.github.com>
2023-12-22 14:39:07 +0900

    .

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import "./App.css";
 //import ChildTwoComponent from "./ChildTwoComponent";
 import { ThemeProvider } from "./ThemeContext";
 import ThemeToggleButton from "./ThemeToggleButton";
-import YTBAPI from "./YTBAPI";
+//import YTBAPI from "./YTBAPI";
+import Quiz from "./Quiz";
 
 //Provider : 해당컴포넌트를 통해서 ThemeContext안에 있는 컴포넌트들한테 객체 값을 공유하고 읽을 수 있도록 해줌
 function App() {
   return (
     <div>
-      <YTBAPI />
+      {/* <YTBAPI /> */}
+      <Quiz />
     </div>
   );
 }
